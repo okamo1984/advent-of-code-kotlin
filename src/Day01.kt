2125b5ef5d8ae0334068658732e090043e1935bb
okamo <okamo1984@gmail.com>
2022-12-01 22:13:49 +0900

    Use utility for simplest code

--- src/Day01.kt
+++ src/Day01.kt
@@ -10,43 +10,33 @@ fun main() {
 
 fun part1() {
     // https://kotlination.com/read-file-kotlin/
-    val bufferedReader = File("src", "Day01.txt").bufferedReader()
-    bufferedReader.use {
-        var max = 0
-        var candidate = 0
-        while (true) {
-            val line = it.readLine()?.trimNewLine() ?: break
-            if (line != "") {
-                candidate += line.toInt()
-            } else {
-                if (candidate > max) {
-                    max = candidate
-                }
-                candidate = 0
-            }
+    var max = 0
+    var candidate = 0
+    for (line in readInput("Day01")) {
+        if (line != "") {
+            candidate += line.toInt()
+            continue
         }
         if (candidate > max) {
             max = candidate
         }
-        println(max)
+        candidate = 0
     }
+    println(max)
 }
 
 fun part2() {
-    val bufferedReader = File("src", "Day01.txt").bufferedReader()
-    bufferedReader.useLines {lines ->
-        val calories = mutableListOf<Int>()
-        var sum = 0
-        for (line in lines) {
-            val calorie = line.trimNewLine()
-            if (calorie != "") {
-                sum += calorie.toInt()
-            } else {
-                calories.add(sum)
-                sum = 0
-            }
+    val calories = mutableListOf<Int>()
+    var sum = 0
+    for (line in readInput("Day01")) {
+        val calorie = line.trimNewLine()
+        if (calorie != "") {
+            sum += calorie.toInt()
+        } else {
+            calories.add(sum)
+            sum = 0
         }
-        val top3 = calories.sortedDescending().slice(0..2)
-        println(top3.sum())
     }
+    val top3 = calories.sortedDescending().slice(0..2)
+    println(top3.sum())
 }
